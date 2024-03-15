62f6b66f3cccbc7a42aa43e373d9b28c7d25eb4e
Sven Olding <sven.olding@vwgis.de>
2024-03-15 14:18:07 +0100

    fix eslint issues

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -11,7 +11,7 @@ export default function GameOver({ winner, onRestart }) {
         <div id="game-over">
             <h2>Game Over!</h2>
             {winner && <p>{winner} won!</p>}
-            {!winner && <p>It's a draw!</p>}
+            {!winner && <p>It&apos;s a draw!</p>}
             <p>
                 <button onClick={() => onRestart()}>Rematch!</button>
             </p>
